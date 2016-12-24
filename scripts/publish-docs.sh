7422b5d227dcb1bb54a2cd032b30fae17058a9b5
Christopher J. Woodall <chris.j.woodall@gmail.com>
2016-12-24 01:13:07 -0500

    Updating docs

---
+++ scripts/publish-docs.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+cargo doc --no-deps
+mkdocs gh-deploy
+git checkout gh-pages
+cp -r target/doc .
+git add doc
+git commit --amend --no-edit
+git push -f origin gh-pages
+git checkout master
