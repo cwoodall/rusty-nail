b5affd5d0949961fdc6788aa22dee1b0676a04fe
Christopher J. Woodall <chris.j.woodall@gmail.com>
2017-01-02 23:08:37 -0500

    Deployed 9120b70 with MkDocs version: 0.16.1

---
+++ doc/implementors/rusty_nail/dispenser/trait.Dispenser.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["rusty_nail"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
